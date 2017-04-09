7bf43ea3f9b325773aeb28988beed4504d3d662a
inbarshani <inbar.shani.il@gmail.com>
2017-04-09 14:23:09 +0200

    almost working; need to fix precedence of unary

--- src/com/inbar/Main.java
+++ src/com/inbar/Main.java
@@ -1,11 +1,14 @@
 package com.inbar;
 
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
-    static Map<String, Integer> symbols = new HashMap<>();
-    static Map<Integer, ArrayList> operators = new HashMap();
+    static Map<String, Integer> symbols = new LinkedHashMap<>();
+    static Map<Integer, Pattern> operators = new HashMap();
+    static Pattern assignment_operators = Pattern.compile("\\+=|-=|\\*=|/=");
 
     public static void main(String[] args) {
         initOperators();
@@ -22,14 +25,11 @@ public class Main {
     }
 
     static void initOperators() {
-        ArrayList<String> ops_1 = new ArrayList<>();
-        ops_1.add("+");
-        ops_1.add("-");
-        operators.put(0, ops_1);
-        ArrayList<String> ops_2 = new ArrayList<>();
-        ops_2.add("*");
-        ops_2.add("/");
-        operators.put(1, ops_2);
+        // init equal operators
+        operators.put(-1,assignment_operators);
+        operators.put(0, Pattern.compile("[\\+-]"));
+        operators.put(1, Pattern.compile("[\\*/]"));
+        operators.put(2, Pattern.compile("\\+\\+[a-zA-Z]|--[a-zA-Z]|[a-zA-Z]\\+\\+|[a-zA-Z]--"));
     }
 
     static String formatResults() {
@@ -37,7 +37,7 @@ public class Main {
         Iterator it = symbols.entrySet().iterator();
         while (it.hasNext()) {
             Map.Entry pair = (Map.Entry)it.next();
-            results.append(pair.getKey() + " = " + pair.getValue());
+            results.append(pair.getKey() + "=" + pair.getValue());
             results.append(",");
         }
         // replace last , with )
@@ -49,27 +49,37 @@ public class Main {
     static void parseLine(String inputLine) {
         Scanner scanner = new Scanner(inputLine);
         String symbol = scanner.next();
-        scanner.next(); // skip equal sign
-        String expression = scanner.nextLine();
-        //System.out.println("Calculating symbol "+symbol+" with expression "+expression);
-        int val = parseOperatorByPriority(expression,0);
+        String equalOp = scanner.next();
+        // check if it's a simple equation, or an operator
+        int val = 0;
+        if (assignment_operators.matcher(equalOp).matches()) {
+            if (symbols.get(symbol) == null)
+                throw new RuntimeException("Use of symbol that wasn't defined: "+symbol);
+            val = parseOperatorByPriority(inputLine,-1);
+        }
+        else {
+            String expression = scanner.nextLine();
+            val = parseOperatorByPriority(expression, 0);
+        }
         symbols.put(symbol, val);
     }
 
     private static int parseOperatorByPriority(String expression, int priority) {
         //System.out.println("evaluating expression "+expression+" for operators of priority "+priority);
-        ArrayList ops = operators.get(priority);
+        Pattern ops = operators.get(priority);
         if (ops == null) // no more operators to scan
             return eval(expression);
         // find operators of this priority
         Scanner scanner = new Scanner(expression);
         StringBuilder left_expression = new StringBuilder(), right_expression = new StringBuilder();
         String op = scanner.next();
-        while ((ops.indexOf(op) == -1) && scanner.hasNext()) {
+        Matcher matcher = ops.matcher(op);
+        while ((!matcher.matches()) && scanner.hasNext()) {
             if (left_expression.length() > 0)
                 left_expression.append(" ");
             left_expression.append(op);
             op = scanner.next();
+            matcher = ops.matcher(op);
         }
         if (scanner.hasNext()) {
             right_expression.append(scanner.nextLine());
@@ -78,39 +88,55 @@ public class Main {
             return evalOp(left_val, op, right_val);
         }
         else {
-            if (ops.indexOf(op) == -1) {
+            if (!matcher.matches()) {
                 if (left_expression.length() > 0)
                     left_expression.append(" ");
                 left_expression.append(op);
+                return parseOperatorByPriority(left_expression.toString(), priority + 1);
             }
-            return parseOperatorByPriority(left_expression.toString(), priority + 1);
+            else {
+                // should only be a unary operator, need to break it down
+                if (left_expression.length() > 0)
+                    throw new RuntimeException("Unrecognized expression syntax: "+expression);
+                // extract the variable
+
+                String var = op.replace("++", "").replace("--","");
+                String unary_op = op.replace(var, "");
+                int left_val = eval(var);
+                int right_val = 1;
+                int val = evalOp(left_val, unary_op, right_val);
+                symbols.put(var, val);
+                return val;
+            }
         }
     }
 
     private static int eval(String expression) {
-        //System.out.println("eval expression "+expression);
         int val;
         try {
             val = Integer.parseInt(expression);
         }
         catch(Exception ex) {
             // expression is not a parsable number, it should be a symbol
-            //System.out.println("Lookup symbol "+expression);
             val = symbols.get(expression);
         }
-        //System.out.println("eval expression "+expression+" to "+val);
         return val;
     }
 
     private static int evalOp(int left_val, String op, int right_val) throws RuntimeException {
-        //System.out.println("eval operator "+op+" with values "+left_val+","+right_val);
         switch(op) {
+            case "++":
+            case "+=":
             case "+":
                 return (left_val + right_val);
+            case "--":
+            case "-=":
             case "-":
                 return (left_val - right_val);
+            case "*=":
             case "*":
                 return (left_val * right_val);
+            case "/=":
             case "/":
                 return (left_val / right_val);
             default:
--- test/com/inbar/MainTest.java
+++ test/com/inbar/MainTest.java
@@ -1,7 +1,11 @@
 package com.inbar;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
 
 /**
  * Created by 2 on 09/04/2017.
@@ -12,39 +16,101 @@ class MainTest {
         Main.initOperators();
     }
 
+    @AfterEach
+    void tearDown() {
+        Main.symbols = new HashMap<>();
+    }
+
     @Test
     public void testOneLine() {
         Main.parseLine("i = 5");
-        assertEquals(5, Main.symbols.get("i").intValue());
+        int i = 5;
+        assertEquals(i, Main.symbols.get("i").intValue());
+        i = 5 * 2;
         Main.parseLine("i = 5 * 2");
-        assertEquals(10, Main.symbols.get("i").intValue());
+        assertEquals(i, Main.symbols.get("i").intValue());
+        i = 5 + 2 * 9;
         Main.parseLine("i = 5 + 2 * 9");
-        assertEquals(23, Main.symbols.get("i").intValue());
+        assertEquals(i, Main.symbols.get("i").intValue());
+        i = 105 - 13 * 80;
         Main.parseLine("i = 105 - 13 * 80");
-        assertEquals(-935, Main.symbols.get("i").intValue());
+        assertEquals(i, Main.symbols.get("i").intValue());
     }
 
     @Test
     public void testVarNoDependency() {
+        int i = 5 * 2;
+        int j = 5 + 2 * 5 - 9;
         Main.parseLine("i = 5 * 2");
         Main.parseLine("j = 5 + 2 * 5 - 9");
-        assertEquals(10, Main.symbols.get("i").intValue());
-        assertEquals(6, Main.symbols.get("j").intValue());
+        assertEquals(i, Main.symbols.get("i").intValue());
+        assertEquals(j, Main.symbols.get("j").intValue());
     }
 
     @Test
     public void testVarWithDependency() {
+        int i = 5 * 4 - 10;
+        int j = 5 + i / 5 - 9;
         Main.parseLine("i = 5 * 4 - 10");
-        Main.parseLine("j = 5 + i * 5 - 9");
-        assertEquals(10, Main.symbols.get("i").intValue());
-        assertEquals(46, Main.symbols.get("j").intValue());
+        Main.parseLine("j = 5 + i / 5 - 9");
+        assertEquals(i, Main.symbols.get("i").intValue());
+        assertEquals(j, Main.symbols.get("j").intValue());
     }
 
     @Test
-    public void testVarWithDependency() {
-        Main.parseLine("i = 5 * 4 - 10");
-        Main.parseLine("j = 5 + i * 5 - 9");
-        assertEquals(10, Main.symbols.get("i").intValue());
-        assertEquals(46, Main.symbols.get("j").intValue());
+    public void testVarWithAssignment() {
+        int i = 5;
+        i += 5;
+        int j = 7;
+        j *= i;
+        int k = 20;
+        k -= i;
+        int x = 10;
+        x /= i;
+
+        Main.parseLine("i = 5");
+        Main.parseLine("i += 5");
+        Main.parseLine("j = 7");
+        Main.parseLine("j *= i");
+        Main.parseLine("k = 20");
+        Main.parseLine("k -= i");
+        Main.parseLine("x = 10");
+        Main.parseLine("x /= i");
+
+        assertEquals(i, Main.symbols.get("i").intValue());
+        assertEquals(j, Main.symbols.get("j").intValue());
+        assertEquals(k, Main.symbols.get("k").intValue());
+        assertEquals(x, Main.symbols.get("x").intValue());
+    }
+
+    @Test
+    public void testVarWithUnary() {
+        int i = 20 - 10 / 2;
+        int j = i++ / 3;
+
+        Main.parseLine("i = 20 - 10 / 2");
+        Main.parseLine("j = i++ / 3");
+        assertEquals(i, Main.symbols.get("i").intValue());
+        assertEquals(j, Main.symbols.get("j").intValue());
+    }
+
+    @Test
+    public void testTaboolaExample() {
+        int i = 0;
+        int j = ++i;
+        int x = i++ + 5;
+        int y = 5 + 3 * 10;
+        i += y;
+
+        Main.parseLine("i = 0");
+        Main.parseLine("j = ++i");
+        Main.parseLine("x = i++ + 5");
+        Main.parseLine("y = 5 + 3 * 10");
+        Main.parseLine("i += y");
+        assertEquals("(i=37,j=1,x=6,y=35)", Main.formatResults());
+        assertEquals(i, Main.symbols.get("i").intValue());
+        assertEquals(j, Main.symbols.get("j").intValue());
+        assertEquals(x, Main.symbols.get("x").intValue());
+        assertEquals(y, Main.symbols.get("y").intValue());
     }
 }
